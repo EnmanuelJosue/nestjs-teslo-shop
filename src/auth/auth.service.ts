91ba3998c77fdf1d47a8bb958e1e64bde6e1f7e1
EnmanuelJosue <enmanueljosue2612@hotmail.com>
2023-06-26 10:30:08 -0400

    implements json jwt

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,86 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { LoginUserDto, CreateUserDto } from './dto/index';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService
+  ) { }
+
+  async create(createAuthDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createAuthDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      });
+      await this.userRepository.save(user);
+      delete user.password;
+      return user;
+    } catch (error) {
+      this.handleDBerrors(error);
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto){
+    const { password, email } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select: { email: true, password: true }
+    })
+    if (!user) throw new UnauthorizedException("User or password doesnt match")
+    
+    const matchPassword = bcrypt.compareSync(password, user.password);
+
+    if (matchPassword) {
+      delete user.password;
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email })
+      }
+    };
+     
+    throw new UnauthorizedException("User or password doesnt match")
+  }
+
+  private getJwtToken( payload: JwtPayload){
+    const token = this.jwtService.sign( payload );
+    return token;
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: any) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  private handleDBerrors(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail)
+
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs')
+  }
+}
